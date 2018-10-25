47f99460d92c2de313a9c89bec17f8834827cf46
Durga Prasad Das (Digital) <DU394084@wipro.com>
2018-10-25 11:37:21 +0530

    util new file added

---
+++ webcrawler/src/main/java/code/util/Util.java
@@ -0,0 +1,32 @@
+package code.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Util {
+
+	 /**
+	 * @param url
+	 * @return String
+	 * this will take url as input and returns domain as string
+	 * 
+	 */
+	public static String getDomain(String url) {
+		 	String domain="";
+			Pattern pattern = Pattern.compile("(http?://)([^:^/]*)(:\\d*)?(.*)?");
+			Matcher matcher = pattern.matcher(url);
+		 try {
+		
+
+			matcher.find();
+			//return domain name
+			domain = matcher.group(2);
+			
+		 }catch(Exception e) {
+			// System.out.println("Invalid URL"+url);
+			 
+		 }
+			return domain;
+}
+}
+
